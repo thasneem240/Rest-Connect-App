753c555f204914a0275801748d859d9f42dfc016
Thasneem Abdul Saleem Mohamed Thasneem <abdulsal@student.curtin.edu.au>
2022-10-02 18:11:14 +0530

    Created RemoteUtilities Class

---
+++ app/src/main/java/com/assignment/part_A/RemoteUtilities.java
@@ -0,0 +1,102 @@
+package com.assignment.part_A;
+
+import android.app.Activity;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+
+public class RemoteUtilities
+{
+
+    public static RemoteUtilities remoteUtilities = null;
+
+    private Activity uiActivity;
+
+    private RemoteUtilities(Activity uiActivity)
+    {
+        this.uiActivity = uiActivity;
+    }
+
+    public static RemoteUtilities getInstance(Activity uiActivity)
+    {
+        if (remoteUtilities == null) {
+            remoteUtilities = new RemoteUtilities(uiActivity);
+        }
+
+        remoteUtilities.setUiActivity(uiActivity);
+
+        return remoteUtilities;
+    }
+
+    public void setUiActivity(Activity uiActivity)
+    {
+        this.uiActivity = uiActivity;
+    }
+
+
+    public HttpURLConnection openConnection(String urlString)
+    {
+        HttpURLConnection connection = null;
+
+        try
+        {
+            URL url = new URL(urlString);
+            connection = (HttpURLConnection) url.openConnection();
+        }
+        catch (MalformedURLException e)
+        {
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        if (connection == null)
+        {
+            uiActivity.runOnUiThread(new Runnable()
+            {
+                @Override
+                public void run()
+                {
+                    Toast.makeText(uiActivity, "Check Internet", Toast.LENGTH_LONG).show();
+                }
+            });
+
+        }
+
+        return connection;
+    }
+
+    public boolean isConnectionOkay(HttpURLConnection conn)
+    {
+        try
+        {
+            if (conn.getResponseCode() == HttpURLConnection.HTTP_OK)
+            {
+                return true;
+            }
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+
+            uiActivity.runOnUiThread(new Runnable()
+            {
+                @Override
+                public void run()
+                {
+                    Toast.makeText(uiActivity, "Problem with API Endpoint", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+
+        return false;
+    }
+
+}
